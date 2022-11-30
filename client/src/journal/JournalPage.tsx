30d9414b1b43a35da3f1818f5fc4f34369c8f8ab
Dylan Powers <dylandpowers@gmail.com>
2022-11-29 22:58:29 -0500

    Basic working journal display

--- client/src/journal/JournalPage.tsx
+++ client/src/journal/JournalPage.tsx
@@ -1,9 +1,30 @@
-import { List, TextField } from "@mui/material";
+import { useQuery } from "@apollo/client";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Skeleton,
+  TextField,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import { SaveButton } from "../components";
+import { GET_JOURNAL_ENTRIES } from "./queries";
+
+interface Entry {
+  id: string;
+  text: string;
+  categories: [string];
+  date: Date;
+}
+
+interface EntryData {
+  entries: [Entry];
+}
 
 const JournalPage: React.FC = () => {
+  const { data, loading } = useQuery<EntryData>(GET_JOURNAL_ENTRIES);
+
   return (
     <Box>
       <TextField
@@ -20,7 +41,17 @@ const JournalPage: React.FC = () => {
           disabled={false}
         />
       </Box>
-      <List></List>
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <List>
+          {data?.entries.map((e) => (
+            <ListItem key={e.id}>
+              <ListItemText>{e.text}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      )}
     </Box>
   );
 };
